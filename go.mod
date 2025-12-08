d4bd7d9d9c258c93719188bb60d55af2ebd1c6b7
Patrick Thum <4435508+pthum@users.noreply.github.com>
2025-12-08 18:33:33 +0100

    add something to have a go sum file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pthum/chart-test
 
 go 1.25.5
+
+require github.com/go-openapi/testify/v2 v2.0.2
--- main_test.go
+++ main_test.go
@@ -3,6 +3,8 @@ package charttest_test
 import (
 	"os"
 	"testing"
+
+	"github.com/go-openapi/testify/v2/assert"
 )
 
 func TestEnvs(t *testing.T) {
@@ -14,4 +16,6 @@ func TestEnvs(t *testing.T) {
 	if sec == "" {
 		t.Errorf("expected sec to be set, but was: '%v'", sec)
 	}
+	assert.NotEmpty(t, sec)
+	assert.NotEmpty(t, en)
 }
